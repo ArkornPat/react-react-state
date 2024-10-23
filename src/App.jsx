b4dc3b0ac190c651c04398bc2f2129c979a725fa
Arkorn Pattanavibul <arkorn.pat@gmail.com>
2024-10-23 13:23:22 +0700

    feat: add useState&event in button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const[textGreeting, setTextGreeting] = useState("Greeting")
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{textGreeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={()=>setTextGreeting("สวัสดี!")} >สวัสดี!</button>
+        <button onClick={()=>setTextGreeting("Hi!")} >Hi!</button>
+        <button onClick={()=>setTextGreeting("你好!")} >你好!</button>
       </div>
     </div>
   );
